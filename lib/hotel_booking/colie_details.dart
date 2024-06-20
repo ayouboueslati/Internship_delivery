88397a0c1d608d89e1bd7e9e5d119248a5cf385f
ayouboueslati <ayoubweslati00@gmail.com>
2024-06-20 14:35:27 +0100

    first commit with some changes add some updates

---
+++ lib/hotel_booking/colie_details.dart
@@ -0,0 +1,523 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../fitness_app/fitness_app_theme.dart';
+
+class HotelDetails extends StatelessWidget {
+  final String completeName;
+  final int phone;
+  final String address;
+  final String colieState;
+  final String time;
+  final String barcode;
+  final int amount;
+  final String recipientInfo;
+  final String supplierInfo;
+  final activeColor = Colors.blue[800]!;
+  final inactiveColor = Colors.grey[600]!;
+
+  HotelDetails({
+    required this.completeName,
+    required this.phone,
+    required this.address,
+    required this.colieState,
+    required this.time,
+    required this.barcode,
+    required this.amount,
+    required this.recipientInfo,
+    required this.supplierInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            AppBar(
+              backgroundColor: Colors.transparent,
+              automaticallyImplyLeading:
+                  true, // Set to true if you want a back button
+              elevation: 0.0,
+              flexibleSpace: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16 - 8.0, 16, 12 - 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Colis details',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: FitnessAppTheme.fontName,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 22 + 6 - 6,
+                              letterSpacing: 1.2,
+                              color: FitnessAppTheme.darkerText,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      GridView.count(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        crossAxisCount: 2,
+                        childAspectRatio: 1,
+                        children: <Widget>[
+                          buildCard(Icons.qr_code, 'code à barre', barcode),
+                          buildClickableCard(
+                              Icons.phone,
+                              'Telephone',
+                              phone.toString(),
+                              'Appeler',
+                              () => _makePhoneCall(phone)),
+                          buildClickableCard(
+                              Icons.location_on,
+                              'Addresse',
+                              address,
+                              'Open in maps',
+                              () => _openInMaps(address)),
+                          buildCard(Icons.person, 'Nom complet', completeName),
+                          buildCard(
+                              Icons.access_time, 'état de Colie', colieState,
+                              cardColor: getColieStateColor(colieState)),
+                          buildCard(Icons.schedule, 'Temps', time),
+                          buildCard(Icons.money, 'Montant', '$amount TND'),
+                          buildCard(Icons.person_pin, 'Destinataire \nInfo',
+                              recipientInfo),
+                          buildCard(Icons.supervisor_account,
+                              'Fournisseur \nInfo', supplierInfo),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  get gr => Expanded(
+      child: Container(
+          padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+          child: GridView.count(
+              crossAxisSpacing: 6,
+              mainAxisSpacing: 6,
+              crossAxisCount: 2,
+              childAspectRatio: .9999,
+              children: <Widget>[
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(
+                              Icons.qr_code, 'code à barre', barcode),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildClickableDetailRow(
+                              Icons.phone,
+                              'Telephone',
+                              phone.toString(),
+                              'Appeler',
+                              () => _makePhoneCall(phone)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildClickableDetailRow(
+                              Icons.location_on,
+                              'Addresse',
+                              address,
+                              'Open in maps',
+                              () => _openInMaps(address)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(
+                              Icons.person, 'Nom complet', completeName),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    color: colieState == 'in stock'
+                        ? Colors.indigoAccent[100]
+                        : colieState == 'in return'
+                            ? Colors.red
+                            : colieState == 'delivered'
+                                ? Colors.green
+                                : Colors.grey, // the default color
+
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(
+                              Icons.access_time, 'état de Colie', colieState),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(Icons.schedule, 'Temps', time),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(Icons.money, 'Montant', '$amount TND'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(Icons.person_pin,
+                              'Destinataire \nInfo', recipientInfo),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Card(
+                    elevation: 20,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          buildDetailRow(Icons.supervisor_account,
+                              'Fournisseur \nInfo', supplierInfo),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ])));
+
+  Widget buildCustomCard(double width, double height, double borderRadius) {
+    return Card(
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Container(
+        width: width,
+        height: height,
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildDetailRow(Icons.person, 'Nom ', completeName),
+
+              // Add more content here as needed
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCard(IconData iconData, String title, String detail,
+      {Color? cardColor}) {
+    return Card(
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      color: cardColor ?? Colors.white, // Default color if not specified
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              iconData,
+              size: 40,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 8),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              detail,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildClickableCard(IconData iconData, String title, String detail,
+      String actionText, VoidCallback onPressed) {
+    return Card(
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              iconData,
+              size: 40,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 8),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              detail,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 8),
+            InkWell(
+              onTap: onPressed,
+              child: Text(
+                actionText,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Color getColieStateColor(String colieState) {
+    switch (colieState) {
+      case 'in stock':
+        return Colors.indigoAccent[100]!;
+      case 'in return':
+        return Colors.red;
+      case 'delivered':
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  Widget buildDetailRow(IconData iconData, String title, String detail) {
+    return Center(
+      //padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(
+                iconData,
+                size: 40,
+                color: Colors.blue,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                detail,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildClickableDetailRow(IconData iconData, String title, String detail,
+      String actionText, VoidCallback onPressed) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(
+                iconData,
+                size: 30,
+                color: Colors.blue,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                detail,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 4),
+              InkWell(
+                onTap: onPressed,
+                child: Text(
+                  actionText,
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _makePhoneCall(int phone) async {
+    final url = 'tel:$phone';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  _openInMaps(String address) async {
+    final url = 'https://www.google.com/maps/search/?api=1&query=$address';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
---
+++ lib/scan.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+import 'fitness_app/fitness_app_theme.dart';
+
+class Scan extends StatefulWidget {
+  @override
+  _ScanState createState() => _ScanState();
+  const Scan({Key? key, required this.animationController}) : super(key: key);
+  final AnimationController? animationController;
+}
+
+class _ScanState extends State<Scan> {
+  String _scanBarcode = 'Unknown';
+  Animation<double>? topBarAnimation;
+  double topBarOpacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> startBarcodeScanStream() async {
+    FlutterBarcodeScanner.getBarcodeStreamReceiver(
+            '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
+        .listen((barcode) => print(barcode));
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: FitnessAppTheme.white.withOpacity(topBarOpacity),
+        automaticallyImplyLeading:
+            false, // Set to true if you want a back button
+        elevation: 0.0,
+        flexibleSpace: CustomScrollView(
+          slivers: <Widget>[
+            SliverPadding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top,
+                left: 6,
+                right: 6,
+                bottom: 2 - 8.0 * topBarOpacity,
+              ),
+              sliver: SliverAppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor:
+                    FitnessAppTheme.white.withOpacity(topBarOpacity),
+                elevation: 0.0,
+                flexibleSpace: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Scanner',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: FitnessAppTheme.fontName,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 22 + 6 - 6 * topBarOpacity,
+                              letterSpacing: 1.2,
+                              color: FitnessAppTheme.darkerText,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                child: ElevatedButton(
+                  onPressed: () => scanBarcodeNormal(),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    foregroundColor: Color.fromARGB(255, 8, 12, 229),
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 22,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 34),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    elevation: 8,
+                    shadowColor:
+                        Color.fromARGB(255, 21, 0, 255).withOpacity(0.6),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.barcode_reader), // Icon added here
+                      SizedBox(
+                          width:
+                              15), // Adjust the spacing between the icon and text
+                      Text('Barcode scan'),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 11, horizontal: 50),
+                child: ElevatedButton(
+                  onPressed: () => scanQR(),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    foregroundColor: Color.fromARGB(255, 51, 0, 255),
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 22,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 76),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    elevation: 8,
+                    shadowColor:
+                        Color.fromARGB(255, 51, 0, 255).withOpacity(0.6),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.qr_code), // Icon added here
+                      SizedBox(
+                          width:
+                              15), // Adjust the spacing between the icon and text
+                      Text('QR scan'),
+                    ],
+                  ),
+                ),
+              ),
+              /* Padding(
+                padding: EdgeInsets.symmetric(vertical: 11, horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: () => startBarcodeScanStream(),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: AppTheme.darkText,
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 22,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 26),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    elevation: 11,
+                    shadowColor: Colors.grey.withOpacity(0.9),
+                  ),
+                  child: const Text('Barcode scan stream'),
+                ),
+              ),*/
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  'Scan result: $_scanBarcode\n',
+                  style: const TextStyle(fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
